f4ef2cef30ece687a8420c5c2e47a02bf0e5235d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-03-27 15:26:14 +0900

    note

--- 0327.rb
+++ 0327.rb
@@ -3,19 +3,17 @@
 ## はじめに
 今回は前回記事の[【初学者】AWSハンズオン①VPC構築](https://qiita.com/i3no29/items/3c36ee1fe93d5c7f6f0d)の続きから、__仮想サーバ([EC2](https://aws.amazon.com/jp/ec2/?ec2-whats-new.sort-by=item.additionalFields.postDateTime&ec2-whats-new.sort-order=desc))__についてのハンズオン記事を書いていきます。
 
-##概要
-[EC2](https://aws.amazon.com/jp/ec2/?ec2-whats-new.sort-by=item.additionalFields.postDateTime&ec2-whats-new.sort-order=desc)（＝Amazon　Elastic　Compute Cloudの略称）
-AWS上に仮想サーバを構築することが出来る技術
-
-EC2によって起動された仮想サーバを『__インスタンス__』
-インスタンスにログインするために『__公開鍵・秘密鍵__』が必要となる
-インスタンスの仮想ファイヤーウォールを『__セキュリティグループ__』
-インスタンスの起動するための情報が含まれているもの『__AMI__』
+##用語
+・[EC2](https://aws.amazon.com/jp/ec2/?ec2-whats-new.sort-by=item.additionalFields.postDateTime&ec2-whats-new.sort-order=desc)（＝Amazon　Elastic　Compute Cloudの略称　Cが2つ続く）AWS上に仮想サーバを構築することが出来る技術
+・EC2によって起動された仮想サーバを『__インスタンス__』
+・インスタンスの起動するための情報が含まれているもの『__AMI__』
+・インスタンスの仮想ファイヤーウォールを『__セキュリティグループ__』
+・インスタンスにログインするために『__キーペア（公開鍵・秘密鍵）__』が必要となる
 
 ##手順概要
-仮想サーバ([EC2](https://aws.amazon.com/jp/ec2/?ec2-whats-new.sort-by=item.additionalFields.postDateTime&ec2-whats-new.sort-order=desc))__（①EC2インスタンスを作成〜②セキュリティグループの作成〜③EC2を起動〜④AMIの作成）__までを構築していく手順をハンズオン形式で公開していきます。
+①AMIの選択〜②インスタンスタイプの選択〜③詳細設定〜④ストレージの追加〜⑤タグの追加〜⑥セキュリティグループの作成〜⑦インスタンスの起動）__までを構築していく手順をハンズオン形式で公開していきます。
 
-## ①EC2インスタンスを作成
+## EC2インスタンスを作成
 ###AWSマネジメントコンソールから【EC2】を選択する
 ![スクリーンショット 2021-03-27 9.59.04.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/694365/a868e70d-7f56-b3f9-c636-1e5200245eec.png)
 左にある【▼インスタンス】の下の【インスタンス】を選択する
@@ -61,3 +59,38 @@ EC2に関連づけるストレージを選択（今回は初期設定のまま�
 ###__セキュリティグループの設定__
 __セキュリティグループ__（＝AWSにおけるファイヤーウォールのこと）
 このセキュリティグループはホワイトリスト方式（記述したもののみ許可する）なので、指定しなければ全て拒否することになります
+![スクリーンショット 2021-03-27 14.13.52.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/694365/300f8b83-ec67-833a-63de-1a64da484f41.png)
+今回の場合では、AWSにアクセスしているIPアドレスに限定したセキュリティグループを設定しています
+
+###インスタンスの起動
+右下に青色の【起動】があるので選択します
+![スクリーンショット 2021-03-27 14.23.53.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/694365/def20140-a2bf-8b77-324e-26b29b774e06.png)
+
+###キーペアの作成について
+EC2は__公開鍵暗号方式__でログイン情報を暗号化しています。そのためログインには、キーペア（公開鍵・秘密鍵）が必要となります（ないと作り直し）。ちなみにキーペアのファイルのダウンロードタイミングはここしかないため、同じインスタンスで二度目のダウンロードはありません。
+![スクリーンショット 2021-03-27 14.35.14.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/694365/a48ffe04-6cb9-3f45-8f5b-7c500a2c9192.png)
+
+###作成ステータス画面
+画面が遷移しそうですが変化がないので、ダッシュボードに移動して確認してみましょう
+![スクリーンショット 2021-03-27 14.35.43.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/694365/96d41cf2-d2c2-28e0-08c3-5a94c62d16da.png)
+
+###EC2ダッシュボードで確認
+【インスタンスEC2の状態】を確認すると実行中となっているので、無事に起動していることがわかります
+![スクリーンショット 2021-03-27 14.58.21.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/694365/10587966-45a0-a0b8-ed31-b94cfe6e7f89.png)
+
+## まとめ
+前回に引き続き__ヨクワカラナイ__なんて方の、少しでも__手を動かすための力__になれたら幸いです。
+
+間違い等ございましたら、お手数ですがご気軽にご教授いただけると幸いです。
+ここまで読んでいただき、誠にありがとうございます。
+
+次回は、今回作成したEC2にアクセスしていきたいと思います
+
+## 参考書籍・記事
+こちらの記事を参考にさせていただております。
+
+【参考書籍】
+[Amazon Web Services パターン別構築・運用ガイド 改](https://www.amazon.co.jp/Amazon-Web-Services-%E3%83%91%E3%82%BF%E3%83%BC%E3%83%B3%E5%88%A5%E6%A7%8B%E7%AF%89%E3%83%BB%E9%81%8B%E7%94%A8%E3%82%AC%E3%82%A4%E3%83%89-%E6%94%B9%E8%A8%82%E7%AC%AC2%E7%89%88-ebook/dp/B07BMQL59H/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&dchild=1&keywords=%E3%81%82%E3%81%BE%E3%81%9E%E3%82%93+Service+%E3%83%91%E3%82%BF%E3%83%BC%E3%83%B3%E5%88%A5&qid=1614865933&sr=8-1)
+[Amazon Web Services 業務システム設計・移行ガイド](https://www.amazon.co.jp/Amazon-Web-Services-%E6%A5%AD%E5%8B%99%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E8%A8%AD%E8%A8%88%E3%83%BB%E7%A7%BB%E8%A1%8C%E3%82%AC%E3%82%A4%E3%83%89-%E4%B8%80%E7%95%AA%E5%A4%A7%E5%88%87%E3%81%AA%E7%9F%A5%E8%AD%98%E3%81%A8%E6%8A%80%E8%A1%93%E3%81%8C%E8%BA%AB%E3%81%AB%E3%81%A4%E3%81%8F-%E4%BD%90%E3%80%85%E6%9C%A8-ebook/dp/B0793JRHYC/ref=pd_vtp_2?pd_rd_w=WcDJz&pf_rd_p=726d0243-39d6-4e23-8ceb-5451be2e842b&pf_rd_r=ZGARH1YNC5B9A7V7SS22&pd_rd_r=f0cfe7b2-9a99-4276-9151-033cadccccec&pd_rd_wg=C7n6P&pd_rd_i=B0793JRHYC&psc=1)
+【サイト】
+[0から始めるAWS入門①：EC2編](https://qiita.com/hiroshik1985/items/f078a6a017d092a541cf)
